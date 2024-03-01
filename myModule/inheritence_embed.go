42527a7b21a65b9de7971baced5fde6043a0b892
rudiansyah <rudiansyahether@gmail.com>
2024-03-01 20:20:15 +0700

    feat(inheritence_embed.go): add Employee struct with embedded Person struct to enable inheritance-like behavior
    chore(inheritence_embed.go): add PrintPersonDetails method to Person struct to print person detailss

--- myModule/inheritence_embed.go
+++ myModule/inheritence_embed.go
@@ -1,6 +1,18 @@
 package myModule
 
+import "fmt"
+
 type Person struct {
 	Name string
 	Age  int
+}
+
+type Employee struct {
+	Person // Embedding Person struct
+	Role   string
+}
+
+func (p *Person) PrintPersonDetails() {
+	fmt.Println("Name :", p.Name)
+	fmt.Println("Age :", p.Age)
 }
